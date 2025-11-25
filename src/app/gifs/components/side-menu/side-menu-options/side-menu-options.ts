dd5694f90bac22279c4278137826168e3843ae57
viklop <viklop7025@gmail.com>
2025-11-25 18:09:39 -0500

    feat: crear signal de menuOptions.

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MenuOption } from './interfaces/menu-option.interface';
 
 @Component({
     selector: 'gifs-side-menu-options',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
     templateUrl: './side-menu-options.html',
     styleUrl: './side-menu-options.css',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+    menuOptions = signal<MenuOption[]>([
+        {
+            label: 'trading',
+            subLabel: 'Gifs populares',
+            icon: 'fa-solid fa-chart-line',
+            route: '/dashboard/trending',
+        },
+        {
+            label: 'Buscador',
+            subLabel: 'Buscar gifs',
+            icon: 'fa-solid fa-chart-glass',
+            route: '/dashboard/search',
+        },
+    ]);
+    // menuOptions: MenuOption[] = [
+    //     {
+    //         label: 'trading',
+    //         subLabel: 'Gifs populares',
+    //         icon: 'fa-solid fa-chart-line',
+    //         route: '/dashboard/trending',
+    //     },
+    //     {
+    //         label: 'Buscador',
+    //         subLabel: 'Buscar gifs',
+    //         icon: 'fa-solid fa-chart-glass',
+    //         route: '/dashboard/search',
+    //     },
+    // ];
+}
