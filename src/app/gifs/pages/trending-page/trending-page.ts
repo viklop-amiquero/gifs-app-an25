c5f4ce2ce7305febf651133bedab4187f8c27a13
viklop <viklop7025@gmail.com>
2025-12-27 11:12:05 -0500

    feat: scroll-state.

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,15 +1,25 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gif-service';
+import { ScrollStateService } from '../../shared/services/scroll-state-service';
 
 @Component({
     selector: 'app-trending-page',
     templateUrl: './trending-page.html',
     styleUrl: './trending-page.css',
 })
-export default class TrendingPage {
+export default class TrendingPage implements AfterViewInit {
     gifService = inject(GifService);
+    scrollStateService = inject(ScrollStateService);
 
     scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+    ngAfterViewInit(): void {
+        const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+        if (!scrollDiv) return;
+
+        scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+    }
 
     onScroll(event: Event) {
         const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -20,6 +30,8 @@ export default class TrendingPage {
         const clientHeight = scrollDiv.clientHeight;
         const scrollHeight = scrollDiv.scrollHeight;
         const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+        this.scrollStateService.trendingScrollState.set(scrollTop);
 
         if (isAtBottom) {
             // Todo siguiente página de gifs
--- src/app/gifs/shared/services/scroll-state-service.ts
+++ src/app/gifs/shared/services/scroll-state-service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ScrollStateService {
-  
+    trendingScrollState = signal(0);
 }
